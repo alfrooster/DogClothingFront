b1303b76fb20c09398c3279687bddeda8f8180f6
Alfred <alfred.ahonen@gmail.com>
2022-12-12 12:52:20 +0200

    app.js takaisin vanhaan

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import './App.css';
-import Login from './Login';
+import ClothingList from './ClothingList';
+import ProducerList from './ProducerList';
 import { Typography, Toolbar, AppBar } from '@mui/material';
+import { Tab } from "@mui/material";
+import { Tabs } from "@mui/material";
 import { useState } from 'react';
 
 function App() {
+  const [tabs, setTabs] = useState("one");
+
+  const handleChange = (event, tabs) => {
+    setTabs(tabs);
+  };
 
   return (
     <div className="App">
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">Welcome to PT-app</Typography>
+          <Typography variant="h6">Welcome to Dog Clothing</Typography>
         </Toolbar>
       </AppBar>
-      <Login />
-      
+      <Tabs value={tabs} onChange={handleChange}>
+          <Tab value="one" label="CLOTHES" />
+          <Tab value="two" label="PRODUCERS" />
+      </Tabs>
+      {tabs === "one" && (
+        <div>
+          <ClothingList />
+        </div>
+      )}
+      {tabs === "two" && (
+        <div>
+          <ProducerList />
+        </div>
+      )}
+
     </div>
-  );
+  )
 }
 
 export default App;
